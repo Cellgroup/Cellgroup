a78f4a8b0b8ea4b45f95bfa6bb04f6321e8ea109
Guido Putignano <63144542+guidoputignano@users.noreply.github.com>
2024-08-31 16:50:05 +0200

    tau and distance matrix added

--- notebooks/cellgroup/clustering_utils.py
+++ notebooks/cellgroup/clustering_utils.py
@@ -95,7 +95,8 @@ def Dspectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=4
     
     nbrs = NearestNeighbors(n_neighbors=n_neighbors, metric='euclidean', n_jobs=-1).fit(coords_normalized)
     distances, indices = nbrs.kneighbors(coords_normalized)
-    
+
+    # Efficiently compute the sparse similarity matrix
     sigma = np.mean(distances, axis=1)
     rows = np.repeat(np.arange(coords_normalized.shape[0]), n_neighbors)
     cols = indices.ravel()
@@ -130,7 +131,110 @@ def Dspectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=4
     return labels, n_clusters
 
 
-def spectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=50): #guido
+def compute_scaled_fixed_distance(cell_size, scaler):
+    """
+    Compute the scaled fixed distance in the normalized space given the distance in original space.
+
+    Parameters:
+    cell_size (float): Distance between two points in original space.
+    scaler (StandardScaler): The scaler used to normalize the data.
+
+    Returns:
+    float: Distance in the scaled (normalized) space.
+    """
+    # Get the standard deviations used in scaling
+    std_dev = scaler.scale_
+
+    # Calculate the distance between point1 and point2 in original space
+    distance_original = cell_size
+
+    # Calculate the scaling factor
+    scaling_factor = np.sqrt(np.sum(std_dev ** 2))
+
+    # Scale the fixed distance
+    distance_scaled = distance_original / scaling_factor
+
+    return distance_scaled
+
+def spectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=40, plot=False, min_cluster=0, tau_percentile=0, cell_size=0):
+
+    """
+    Perform spectral clustering on a large DataFrame with X and Y coordinates, removing links with a distance lower than tau.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame with 'X' and 'Y' columns
+    n_clusters (int, optional): Number of clusters to form. If None, auto-determined.
+    n_neighbors (int): Number of nearest neighbors for sparse similarity matrix
+    max_eigenvectors (int): Maximum number of eigenvectors to compute
+    tau_percentile (float): Percentile of distances to use as threshold for removing links
+
+    Returns:
+    numpy.ndarray: Cluster labels for each point
+    """
+
+    coords = df[['X', 'Y']].values
+
+    # Normalize the input data
+    scaler = StandardScaler()
+    coords_normalized = scaler.fit_transform(coords)
+
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors, metric='euclidean', n_jobs=-1).fit(coords_normalized)
+    distances, indices = nbrs.kneighbors(coords_normalized)
+    if cell_size > 0:
+        distance_scaled = compute_scaled_fixed_distance(cell_size, scaler)
+        # distances are infinite when the distance is higher scaled distance
+        distances[distances > distance_scaled] = np.inf
+
+
+    # Determine the distance threshold tau based on a percentile
+    tau = np.percentile(distances, tau_percentile)
+
+    # Efficiently compute the sparse similarity matrix
+    sigma = np.mean(distances, axis=1)
+    rows = np.repeat(np.arange(coords_normalized.shape[0]), n_neighbors)
+    cols = indices.ravel()
+
+    # Gaussian kernel similarity
+    #similarities = np.exp(-distances.ravel()**2 / (2 * sigma[rows]**2))
+    # Laplacian kernel similarity
+    similarities = np.exp(-np.abs(distances.ravel()) / sigma[rows])
+    # Apply the threshold tau to remove links with distance below tau
+    mask = distances.ravel() >= tau  # Create a boolean mask for distances above tau
+    similarities[~mask] = 0  # Set similarities to 0 where distance is below tau
+
+    similarity_matrix = csr_matrix((similarities, (rows, cols)), shape=(coords_normalized.shape[0], coords_normalized.shape[0]))
+    similarity_matrix = (similarity_matrix + similarity_matrix.T) / 2
+    # A diagonal matrix where each diagonal entry represents the sum of similarities (weights) of the edges connected to a node (point). It is effectively a measure of how connected each point is in the graph.
+    degree_matrix = np.array(similarity_matrix.sum(axis=1)).flatten()
+    # The Laplacian matrix is computed as 𝐷 − 𝑊, where 𝐷 is the degree matrix and is the similarity matrix. The Laplacian matrix encodes the graph structure and is crucial for spectral clustering.
+    laplacian = csr_matrix(np.diag(degree_matrix)) - similarity_matrix
+    # The normalized Laplacian is computed to scale the Laplacian by the inverse of the degree matrix, which can make the eigenvalue problem more stable and interpretable.
+    norm_laplacian = laplacian.multiply(csr_matrix(1 / degree_matrix[:, None]))
+
+    # Compute eigenvectors of the normalized Laplacian
+    eigenvalues, eigenvectors = eigsh(norm_laplacian, k=max_eigenvectors, which='SM', return_eigenvectors=True)
+
+    # Determine the number of clusters using auto_spectral_gap
+    if n_clusters is None:
+        n_clusters = auto_spectral_gap(eigenvalues, min_cluster)
+    else:
+        n_clusters = min(n_clusters, len(eigenvalues))
+
+    if plot is True:
+        plot_cdf(eigenvalues)
+        plot_approximate_cdf(eigenvalues)
+        plot_gradients(eigenvalues)
+
+    # Select the appropriate number of eigenvectors
+    eigenvectors = eigenvectors[:, :n_clusters]
+
+    U_normalized = eigenvectors / np.linalg.norm(eigenvectors, axis=1, keepdims=True)
+
+    labels = kmeans(U_normalized, n_clusters)
+
+    return labels, n_clusters
+
+def Gspectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=40, plot=False, min_cluster=0):
     """
     Perform spectral clustering on a large DataFrame with X and Y coordinates.
 
@@ -171,9 +275,14 @@ def spectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=50
 
     # Determine the number of clusters using auto_spectral_gap
     if n_clusters is None:
-        n_clusters = auto_spectral_gap(eigenvalues)
+        n_clusters = auto_spectral_gap(eigenvalues, min_cluster)
     else:
         n_clusters = min(n_clusters, len(eigenvalues))
+
+    if plot is True:
+        plot_cdf(eigenvalues)
+        plot_approximate_cdf(eigenvalues)
+        plot_gradients(eigenvalues)
 
     # Select the appropriate number of eigenvectors
     eigenvectors = eigenvectors[:, :n_clusters]
@@ -184,29 +293,14 @@ def spectral_clustering(df, n_clusters=None, n_neighbors=20, max_eigenvectors=50
 
     return labels, n_clusters
 
-def Dauto_spectral_gap(evals):
-    """
-    The auto_spectral_gap function is designed to determine the optimal number of clusters (cut_point) by analyzing the spectral gap of the eigenvalues (evals) of a matrix, typically the Laplacian matrix in spectral clustering.
-    :param evals: eigenvalues
-    :return: index of evals where the spectral gap is significant
-    """
-    # The gradient provides an approximation of the rate of change of the eigenvalues, which helps identify where the spectral gap (difference between successive eigenvalues) is significant.
-    tmp = np.gradient(evals, edge_order=2) # order 2 for better accuracy
-    tmp = np.gradient(tmp, edge_order=2)
-    tmp = tmp[1:]
-    tmp = tmp + 1e-8  # fix numerical instability
-    pos_point = np.argmax(tmp > 0) # find the first positive point
-    tmp = tmp[pos_point+1:] # Identifying the Subsequent Spectral Gap
-    cut_point = np.argmax(tmp < 0) + 1 + pos_point + 1 # adjustment recalculates the absolute index in the original evals array since tmp was sliced
-    return cut_point
 
-
-def auto_spectral_gap(evals): #guido
+def auto_spectral_gap(evals, min_cluster=0):
     """
     Determines the optimal number of clusters by analyzing the spectral gap of eigenvalues.
 
     Parameters:
     evals (numpy.ndarray): Array of eigenvalues sorted in ascending order.
+    min_cluster (int): Minimum index for the cluster count (default is 0).
 
     Returns:
     int: Index indicating the optimal number of clusters.
@@ -217,52 +311,95 @@ def auto_spectral_gap(evals): #guido
     # Avoid numerical instability by adding a small constant
     second_derivative += 1e-8
 
-    # Find the first point where the curvature becomes positive
-    pos_point = np.argmax(second_derivative > 0)
+    # Find the first positive curvature point
+    positive_indices = np.where(second_derivative > 0)[0]
+    if positive_indices.size == 0:
+        # If no positive curvature, return min_cluster or the last index
+        return max(min_cluster, len(evals) - 1)
+
+    pos_point = positive_indices[0]
+
+    # Find the indices where the second derivative is negative
+    negative_indices = np.where(second_derivative < 0)[0]
 
     # Search for the next significant change in curvature after the first positive point
-    cut_point = pos_point + 1 + np.argmax(second_derivative[pos_point + 1:] < 0)
+    i = 0
+    while True:
+        if i >= len(negative_indices):
+            # If there are no more negative indices, use the last index
+            cut_point = len(evals) - 1
+            break
 
+        # Compute cut_point
+        cut_point = negative_indices[i]
+
+        # If cut_point is valid
+        if cut_point > min_cluster and cut_point > pos_point:
+            cut_point = cut_point
+            break
+
+        # Otherwise, move to the next negative index
+        i += 1
+
+    print("pos_point = " + str(pos_point) + " cut_point = " + str(cut_point))
     return cut_point
 
+def plot_cdf(evals):
+    """
+    Plot the cumulative distribution function (CDF) of eigenvalues.
 
-def Dkmeans(X: np.ndarray, k: int, n_runs: int = 10, max_iters: int = 100) -> np.ndarray:
-    best_labels = None  # store the labels of the best clustering found.
-    best_inertia = np.inf # store the inertia of the best clustering found.
+    Parameters:
+    evals (numpy.ndarray): Array of eigenvalues sorted in ascending order.
+    """
+    values = np.sort(evals)
+    p = 1. * np.arange(len(values)) / (len(values) - 1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(values, p, marker='.', linestyle='none')
+    plt.xlabel('Value')
+    plt.ylabel('Cumulative Probability')
+    plt.title('Cumulative Distribution Function (CDF) of Eigenvalues')
+    plt.grid(True)
+    plt.show()
 
-    for _ in range(n_runs): # run the algorithm multiple times to avoid local minima
-        centroids = X[np.random.choice(X.shape[0], k, replace=False)]
-        
-        for _ in range(max_iters): # run the algorithm for a maximum number of iterations
-            distances = ((X - centroids[:, np.newaxis])**2).sum(axis=2)
-            labels = np.argmin(distances, axis=0)
-            
-            new_centroids = np.zeros_like(centroids)
-            for i in range(k):
-                cluster_points = X[labels == i]
-                if len(cluster_points) > 0:
-                    new_centroids[i] = cluster_points.mean(axis=0)
-                else:
-                    # If a cluster is empty, reinitialize it
-                    new_centroids[i] = X[np.random.choice(X.shape[0])]
-            
-            if np.allclose(centroids, new_centroids):
-                break
-            
-            centroids = new_centroids # update centroids
-        
-        inertia = np.sum((X - centroids[labels])**2) # compute the inertia of the clustering
-        if inertia < best_inertia:
-            best_inertia = inertia
-            best_labels = labels
-    
-    return best_labels
+def plot_approximate_cdf(evals, n_bins=1000):
+    """
+    Plot an approximate cumulative distribution function (CDF) of eigenvalues.
 
+    Parameters:
+    evals (numpy.ndarray): Array of eigenvalues.
+    n_bins (int): Number of bins for the histogram.
+    """
+    values = evals
+    counts, bin_edges = np.histogram(values, bins=n_bins)
+    cdf = np.cumsum(counts) / counts[-1]
+    plt.figure(figsize=(10, 6))
+    plt.plot(bin_edges[1:], cdf, '-')
+    plt.xlabel('Value')
+    plt.ylabel('Cumulative Probability')
+    plt.title('Approximate Cumulative Distribution Function (CDF) of Eigenvalues')
+    plt.grid(True)
+    plt.show()
 
-import numpy as np
+def plot_gradients(evals):
+    """
+    Plot the gradients of eigenvalues to identify the spectral gap.
 
+    Parameters:
+    evals (numpy.ndarray): Array of eigenvalues sorted in ascending order.
+    """
+    gradient1 = np.gradient(evals)
+    gradient2 = np.gradient(gradient1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(gradient1, label='First Gradient')
+    plt.plot(gradient2, label='Second Gradient')
+    plt.xlabel('Index')
+    plt.ylabel('Gradient')
+    plt.title('Gradients of Eigenvalues')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-def kmeans(X: np.ndarray, k: int, n_runs: int = 10, max_iters: int = 100) -> np.ndarray: #guido
+def kmeans(X: np.ndarray, k: int, n_runs: int = 10, max_iters: int = 100) -> np.ndarray:
     """
     Optimized k-means clustering algorithm.
 
@@ -338,13 +475,14 @@ def initialize_centroids(X: np.ndarray, k: int) -> np.ndarray:
     return centroids
 
 
-def Drun_spectral_clustering(input_data, n_clusters: Optional[int] = None, verbose: int = 1):
+def run_spectral_clustering(input_data, n_clusters: Optional[int] = None, verbose: int = 1, plot: bool = False, min_cluster = 0):
     input_data = input_data.values if hasattr(input_data, 'values') else input_data
     if verbose >= 2:
         plot_points(input_data, title='Input data')
-    
+
+
     t1 = time.perf_counter()
-    cluster_labels, n_clusters = spectral_clustering(pd.DataFrame(input_data, columns=['X', 'Y']), n_clusters)
+    cluster_labels, n_clusters = spectral_clustering(pd.DataFrame(input_data, columns=['X', 'Y']), n_clusters, plot=plot, min_cluster=min_cluster)
     processing_time = time.perf_counter() - t1
     
     if verbose >= 1:
@@ -355,48 +493,4 @@ def Drun_spectral_clustering(input_data, n_clusters: Optional[int] = None, verbo
         plot_points(input_data, clusters=clusters, title='Spectral Clustering Results')
     
     results = saveResults(pd.DataFrame(input_data, columns=['X', 'Y']), [], processing_time, clusters)
-    return clusters, results
-
-
-def run_spectral_clustering(input_data, n_clusters: Optional[int] = None, verbose: int = 1): #guido
-    """
-    Perform spectral clustering on input data and optionally visualize the results.
-
-    Parameters:
-    input_data : array-like or pandas.DataFrame
-        The input data to cluster.
-    n_clusters : int, optional
-        The number of clusters to form. If None, the number of clusters is determined automatically.
-    verbose : int
-        The verbosity level. Higher values result in more detailed output and plotting.
-
-    Returns:
-    clusters : list of numpy.ndarray
-        Indices of input data points for each cluster.
-    results : object
-        The result object from the saveResults function, which might contain processed data and statistics.
-    """
-
-    # Ensure input data is in DataFrame format and store it once
-    input_df = pd.DataFrame(input_data if not hasattr(input_data, 'values') else input_data.values, columns=['X', 'Y'])
-
-    if verbose >= 2:
-        plot_points(input_df.values, title='Input data')
-
-    # Start timing and perform spectral clustering
-    t1 = time.perf_counter()
-    cluster_labels, n_clusters = spectral_clustering(input_df, n_clusters)
-    processing_time = time.perf_counter() - t1
-
-    if verbose >= 1:
-        print(f'Total time for processing: {processing_time:.2f} s')
-
-    # Group indices by cluster label efficiently
-    clusters = [np.flatnonzero(cluster_labels == i) for i in range(n_clusters)]
-
-    if verbose >= 2:
-        plot_points(input_df.values, clusters=clusters, title='Spectral Clustering Results')
-
-    # Save results and return
-    results = saveResults(input_df, [], processing_time, clusters)
     return clusters, results
