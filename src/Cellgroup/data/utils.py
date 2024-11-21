1535fb8aec8a7f249249ee018319a650007e7c64
federico-carrara <federico1.carrara@mail.polimi.it>
2024-11-21 14:15:42 +0100

    refac: renamed 'Cellgroup' dir into 'cellgroup', moved util functions to `utils.py`

--- src/Cellgroup/data/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-
-import numpy as np
-import tifffile as tiff
-
-
---
+++ src/cellgroup/data/utils.py
@@ -0,0 +1,49 @@
+import os
+import re
+from enum import Enum
+from pathlib import Path
+
+
+class WellID(Enum):
+    """IDs for the different wells in the dataset."""
+    A05 = "_A05_"
+    A06 = "_A06_"
+    A07 = "_A07_"
+    A08 = "_A08_"
+    A09 = "_A09_"
+    
+class ChannelID(Enum):
+    """IDs for the different channels in the dataset."""
+    Ch1 = "C01"
+    Ch2 = "C02"
+    Ch5 = "C05"
+    Ch6 = "C06"
+    Ch13 = "C13"
+    
+
+def _sort_files_by_time(files: list[str]) -> list[str]:
+    """Sort a list of filenames based on the day and time step encoded in their names.
+    
+    Day is denoted by 'D#xx' and time step by 'Txxxx'.
+    
+    Parameters
+    ----------
+    files list[str]: 
+        List of filenames to be sorted.
+    
+    Returns
+    -------
+    list[str]: 
+        Sorted list of filenames.
+    """
+    def extract_day_and_time(filename: str):
+        """Extract day and time using regex."""
+        match = re.search(r'D#(\d+)_T(\d+)', filename)
+        if match:
+            day = int(match.group(1))
+            time_step = int(match.group(2))
+            return (day, time_step)
+        # Sort invalid entries to the end
+        return (float('inf'), float('inf'))
+
+    return sorted(files, key=extract_day_and_time)
