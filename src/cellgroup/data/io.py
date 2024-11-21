1f319a87ea71c2af6dcf9a1bf6f33d1c1e7c6710
federico-carrara <federico1.carrara@mail.polimi.it>
2024-11-21 16:30:01 +0100

    feat: added possibility to select a slice of time steps

--- src/cellgroup/data/io.py
+++ src/cellgroup/data/io.py
@@ -8,7 +8,7 @@ import numpy as np
 from numpy.typing import NDArray
 from tqdm import tqdm
 
-from cellgroup.data.utils import ChannelID, WellID, _sort_files_by_time
+from cellgroup.data.utils import ChannelID, WellID, _sort_files_by_time, _subsample_timesteps
 
 
 def get_fnames(
@@ -16,8 +16,7 @@ def get_fnames(
     well_ids: Sequence[WellID],
     channel_ids: Sequence[ChannelID],
     img_dim: Literal["2D", "3D"] = "2D",
-    t_steps: Optional[int] = None,
-    every_n_steps: Optional[int] = None,
+    t_steps_subsample: Optional[tuple[int, int, int]] = None,
 ) -> dict[WellID, dict[ChannelID, list[Path]]]:
     """Get the filenames for the given wells and channels.
     
@@ -31,11 +30,8 @@ def get_fnames(
         The IDs of the channels to include.
     img_dim : Literal["2D", "3D"]
         The dimensionality of the images. By default "2D".
-    t_steps : Optional[int]
-        The number of time steps to include.
-        If None, all time steps are taken. By default None.
-    every_n_steps : Optional[int]
-        The number of time steps to include.
+    t_steps_subsample : Optional[tuple[int, int, int]]
+        An interval of time steps to include, in the form (start, end, step).
         If None, all time steps are taken. By default None.
     
     Returns
@@ -45,6 +41,7 @@ def get_fnames(
     """
     assert img_dim == "2D", "Only 2D images are supported for now."
     assert WellID.A05 not in well_ids, "Well A05 not available in the dataset for now."
+    assert len(t_steps_subsample) >= 2, "You need to provide at least (start, end)."
     
     subdir = "slices" if img_dim == "2D" else "stacks"
     fnames_dict = {}
@@ -55,22 +52,9 @@ def get_fnames(
             curr_fnames = [
                 fname for fname in os.listdir(well_subdir) if channel_id.value in fname
             ]
-            # --- select timesteps
             curr_fnames = _sort_files_by_time(curr_fnames)
-            if t_steps:
-                if t_steps > len(curr_fnames):
-                    warn(
-                        message=(
-                            f"You picked {t_steps} timesteps with only"
-                            f" {len(curr_fnames)} available. Taking all of them."
-                        ),
-                        stacklevel=2,
-                    )
-                curr_fnames = curr_fnames[:t_steps]
-            # --- subsample timesteps
-            if every_n_steps:
-                curr_fnames = curr_fnames[::every_n_steps]
-            # --- append to dict
+            if t_steps_subsample is not None:
+                curr_fnames = _subsample_timesteps(curr_fnames, t_steps_subsample)
             fnames_dict[well_id][channel_id] = [
                 Path(os.path.join(well_subdir, fname)) for fname in curr_fnames
             ]
@@ -82,8 +66,7 @@ def load_images(
     well_ids: Sequence[WellID],
     channel_ids: Sequence[ChannelID],
     img_dim: Literal["2D", "3D"] = "2D",
-    t_steps: Optional[int] = None,
-    every_n_steps: Optional[int] = None,
+    t_steps_subsample: Optional[tuple[int, int, int]] = None,
 ) -> dict[WellID, dict[ChannelID, NDArray]]:
     """Load an image from a file.
     
@@ -97,11 +80,8 @@ def load_images(
         The IDs of the channels to include.
     img_dim : Literal["2D", "3D"]
         The dimensionality of the images. By default "2D".
-    t_steps : Optional[int]
-        The number of time steps to include.
-        If None, all time steps are taken. By default None.
-    every_n_steps : Optional[int]
-        The number of time steps to include.
+    t_steps_subsample : Optional[tuple[int, int, int]]
+        An interval of time steps to include, in the form (start, end, step).
         If None, all time steps are taken. By default None.
     
     Returns
@@ -113,9 +93,8 @@ def load_images(
         data_dir=data_dir, 
         well_ids=well_ids, 
         channel_ids=channel_ids, 
-        img_dim=img_dim, 
-        t_steps=t_steps, 
-        every_n_steps=every_n_steps
+        img_dim=img_dim,
+        t_steps_subsample=t_steps_subsample
     )
     images_dict = {}
     for well_id, channel_dict in fnames_dict.items():
--- src/cellgroup/data/utils.py
+++ src/cellgroup/data/utils.py
@@ -1,7 +1,6 @@
-import os
 import re
 from enum import Enum
-from pathlib import Path
+from warnings import warn
 
 
 class WellID(Enum):
@@ -47,3 +46,45 @@ def _sort_files_by_time(files: list[str]) -> list[str]:
         return (float('inf'), float('inf'))
 
     return sorted(files, key=extract_day_and_time)
+
+
+def _subsample_timesteps(
+    fnames: list[str], 
+    t_steps_slice: tuple[int, int, int]
+) -> list[str]:
+    """Subsample filenames given an interval of timesteps.
+    
+    Parameters
+    ----------
+    fnames : list[str]
+        List of filenames to subsample.
+    t_steps_slice : tuple[int, int, int]
+        An interval of time steps to include, in the form (start, end, step).
+    
+    Returns
+    -------
+    list[str]
+        Subsampled list of filenames.
+    """
+    fnames = _sort_files_by_time(fnames)
+    
+    if len(t_steps_slice) == 2:
+        start, end = t_steps_slice
+        step = 1
+    elif len(t_steps_slice) == 3:
+        start, end, step = t_steps_slice
+    
+    if end > len(fnames):
+        warn(
+            message=(
+                f"You picked the {end} timestep with only "
+                f"{len(fnames)} available. Taking last one available."
+            ),
+            stacklevel=2,
+        )
+    if start >= len(fnames):
+        raise ValueError(
+            f"Invalid start timestep {start} with only {len(fnames)} available."
+        )
+     
+    return fnames[slice(start, end, step)]
