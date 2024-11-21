f3f9e95da12cff1351d7942cc1994cf59ca0e9a5
federico-carrara <federico1.carrara@mail.polimi.it>
2024-11-21 14:32:52 +0100

    refac: updated setup.py to be consistent with the new repo structure

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Cellgroup",
+    name="cellgroup",
     version="0.0.1",
     author="Guido Putignano",
     author_email="guido.putignano@gmail.com",
-    url="https://github.com/guidoputignano/guidoputignano",
+    url="https://github.com/guidoputignano/Cellgroup",
     description="A library whose function is to cluster, analyse, and potentially forecast cells in a 2D environment",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=["click", "pytz"], #to write once ocmpleted
-    entry_points={"console_scripts": ["Cellgroup = src.main:main"]},
+    entry_points={"console_scripts": ["cellgroup = src.main:main"]},
 )
